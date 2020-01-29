d0d68b5b77ee1d8ca88337ab7d88c641e7f14799
subhrasmita-ojha <60036690+subhrasmita-ojha@users.noreply.github.com>
2020-01-29 12:51:20 +0530

    Add files via upload

---
+++ InventaryManagementSystem/ProductDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaryManagementSystem
+{
+    class ProductDetail
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+        public string type;
+        public ProductDetail(string Name,double Price,int Quantity,string type)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Quantity = Quantity;
+            this.type = type;
+
+        }
+    }
+}
